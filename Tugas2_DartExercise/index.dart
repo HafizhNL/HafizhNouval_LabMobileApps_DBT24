e25d8d09d451756a0a974d97f1542d9c576575c1
HafizhNL <hafizh.nouval25@gmail.com>
2026-01-21 13:04:58 +0700

    update

---
+++ Tugas2_DartExercise/index.dart
@@ -0,0 +1,74 @@
+
+void main() {
+// 1
+//   var nama = "Hafizh";
+//   int umur = 21;
+//   var sisa = 100 - umur;
+//   print("umur ${nama} sisa ${sisa}");
+  
+// 2
+  //  int a = 4;
+  //  if (a % 2 == 1) {
+  //    print("ganjil");
+  //  } else {
+  //    print("genap");
+  //  }
+
+// 3
+//   List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  
+//   for (int i in a) {
+//     if (i < 5) {
+//       print(i);
+//     }
+//   }
+  
+// 4
+//   int a = 26;
+//   List result = [];
+  
+//   for (int i = 0; i < a; i++) {
+//     print(i);
+//     if (a % i == 0) {
+//       result.add(i);
+//     }
+//   }
+//   print(result);
+  
+// 5
+//   List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+//   List b = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+//   Set result = {}; 
+  
+//   for (int i in a) {
+//     for (int j in b) {
+//       if (i == j) {
+//         result.add(i);
+//       }
+//     }
+//   }
+//   print(result);
+
+// 6
+//   String input = "kasur rusak";
+//   String cleaned = input.replaceAll(' ', '').toLowerCase();
+//   String reversed = cleaned.split('').reversed.join('');
+  
+//   if (cleaned == reversed) {
+//     print("'$input' adalah palindrome");
+//   } else {
+//     print("'$input' bukan palindrome");
+//   }
+  
+// 7
+//   List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   List genap = [];
+  
+//   for (int angka in a) {
+//     if (angka % 2 == 0) {
+//       genap.add(angka);
+//     }
+//   }
+//   print(genap);
+
+}
